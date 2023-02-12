5913b85e0c0843cb72a4bdf6914f723b136c26d4
JustAlittleWolf <54244277+JustAlittleWolf@users.noreply.github.com>
2023-02-12 20:46:33 +0100

    Update LightProviderMixin.java
    Unused mixin but still

--- src/main/java/me/wolfie/methane/mixin/LightProviderMixin.java
+++ src/main/java/me/wolfie/methane/mixin/LightProviderMixin.java
@@ -1,5 +1,6 @@
 package me.wolfie.methane.mixin;
 
+import me.wolfie.methane.Methane;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.light.LightingProvider;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,6 +13,6 @@ public class LightProviderMixin {
 
     @Inject(method = "getLight", at = @At("RETURN"), cancellable = true)
     private void returnGetLight(BlockPos pos, int ambientDarkness, CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(15);
+        if(Methane.ModActive) cir.setReturnValue(15);
     }
 }
