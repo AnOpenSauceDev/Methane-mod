044257a054c3f2471e92f7a3e51f702fe7482d8a
JustAlittleWolf <54244277+JustAlittleWolf@users.noreply.github.com>
2023-02-12 18:51:09 +0100

    Fixed small oopsie
    Changed ModInitializer in Methane.class to ClientModInitializer so that the game doesn't crash

--- src/main/java/me/wolfie/methane/Methane.java
+++ src/main/java/me/wolfie/methane/Methane.java
@@ -1,16 +1,16 @@
 package me.wolfie.methane;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Methane implements ModInitializer {
+public class Methane implements ClientModInitializer {
 
     public static boolean ModActive = true; // for toggles
 
     public static Logger MethaneLogger = LoggerFactory.getLogger("Methane");
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         MethaneLogger.info("Methane has loaded!");
     }
 
