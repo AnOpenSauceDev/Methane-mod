cb2dfe617aee3f28065a970346c6ba0210b95e32
JustAlittleWolf <54244277+JustAlittleWolf@users.noreply.github.com>
2023-02-12 20:20:19 +0100

    Update LightProviderMixin.java

--- src/main/java/me/wolfie/methane/mixin/LightProviderMixin.java
+++ src/main/java/me/wolfie/methane/mixin/LightProviderMixin.java
@@ -1,36 +1,17 @@
 package me.wolfie.methane.mixin;
 
-import me.wolfie.methane.Methane;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.chunk.light.ChunkLightProvider;
 import net.minecraft.world.chunk.light.LightingProvider;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LightingProvider.class)
 public class LightProviderMixin {
 
-    @Shadow
-    private ChunkLightProvider<?, ?> blockLightProvider;
-    @Shadow
-    ChunkLightProvider<?, ?> skyLightProvider;
-
-    /**
-     * @author
-     * @reason
-     */
-    @Overwrite
-    public int getLight(BlockPos pos, int ambientDarkness) {
-        int i = this.skyLightProvider == null ? 0 : this.skyLightProvider.getLightLevel(pos) - ambientDarkness;
-        int j = this.blockLightProvider == null ? 0 : this.blockLightProvider.getLightLevel(pos);
-        if(Methane.ModActive) {
-            return 15;
-        }else {
-            return Math.max(j, i);
-        }
+    @Inject(method = "getLight", at = @At("RETURN"), cancellable = true)
+    private void returnGetLight(BlockPos pos, int ambientDarkness, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(15);
     }
-
 }
