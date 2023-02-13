7c1faa5a0d3a73a40823c81d1040a1270af0aa8d
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2023-02-13 17:51:24 +1000

    add Mod Menu support

--- src/main/java/me/wolfie/methane/Methane.java
+++ src/main/java/me/wolfie/methane/Methane.java
@@ -1,17 +1,21 @@
 package me.wolfie.methane;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Methane implements ClientModInitializer {
+public class Methane implements ModInitializer {
 
     public static boolean ModActive = true; // for toggles
 
     public static Logger MethaneLogger = LoggerFactory.getLogger("Methane");
     @Override
-    public void onInitializeClient() {
+    public void onInitialize() {
         MethaneLogger.info("Methane has loaded!");
+        AutoConfig.register(MethaneSettings.class, JanksonConfigSerializer::new);
     }
 
 }
--- src/main/java/me/wolfie/methane/MethaneSettings.java
+++ src/main/java/me/wolfie/methane/MethaneSettings.java
@@ -1,10 +1,16 @@
 package me.wolfie.methane;
 
-public class MethaneSettings {
-    public static boolean disableAirFog = true;
-    public static boolean disableWaterFog = true;
-    public static boolean disableLavaFog = true;
-    public static boolean disablePowderedSnowFog = true;
-    public static boolean disableThickFog = true; // Idk when exactly this applies
-    public static boolean disableSkyFog = true; // Idk when exactly this applies either
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
+
+@Config(name = "methane")
+public class MethaneSettings implements ConfigData {
+    public static boolean disableAirFog = false; // the fog pass that obscures terrain
+    public static boolean disableWaterFog = false; // the fog layer that tints everything blue
+    public static boolean disableLavaFog = false; // the thing that tints everything orange
+    public static boolean disablePowderedSnowFog = true; // pretty self-explanatory
+    @Comment("(you should probably disable this)")
+    public static boolean disableThickFog = false; // Nether Fog pass
+    public static boolean disableSkyFog = false; // i think this is another fog pass
 }
