b998c5c435c80f47f61e7bdbf9c037b58677bab5
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2023-02-14 07:16:34 +1000

    added fog persistence as an option

--- src/main/java/me/wolfie/methane/MethaneSettings.java
+++ src/main/java/me/wolfie/methane/MethaneSettings.java
@@ -6,6 +6,8 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = "methane")
 public class MethaneSettings implements ConfigData {
+    @Comment("Toggle whether to keep fog settings even with Methane disabled.")
+    public static boolean persistFogSettings = false;
     public static boolean disableAirFog = false; // the fog pass that obscures terrain
     public static boolean disableWaterFog = false; // the fog layer that tints everything blue
     public static boolean disableLavaFog = false; // the thing that tints everything orange
