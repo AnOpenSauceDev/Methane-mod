a35b8430fef62f82ce7215221f729d9eb8bd519a
JustAlittleWolf <54244277+JustAlittleWolf@users.noreply.github.com>
2023-02-16 23:01:32 +0100

    Fix config saving (#23)
    * Reworked Fog Removal
    
    Now works with Sodium
    Changed from @Overwrite mixin to @Inject mixin for cleaner code
    Removed checks for Sodium as they are now unnecessary
    Added changeable variables in MethaneSettings.java to toggle which fog should be hidden (For later ModMenu integration)
    Reworked server-check so that the server doesn't crash if methane is installed (gives a warning tho)
    
    * Fixed small oopsie
    
    Changed ModInitializer in Methane.class to ClientModInitializer so that the game doesn't crash
    
    * Update GameRendererMixin.java
    
    Changed suppression of lightmapTextureManager.update from @Overwrite to @Redirect to make development easier
    
    * Update LightingUpdateMixin.java
    
    Changed the @Overwrite calls to use @Inject for more concise and maintainable code.
    Also removed the updateChunk overwrite completely, as it only removes readLightdata, which has a methane activated check anyways.
    
    * Update LightProviderMixin.java
    
    * Update LightProviderMixin.java
    
    Unused mixin but still
    
    * Persistent settings
    
    changed it so that the settings actually save to the config file and are kept when the game restarts
    also update german translation
    
    * small formatting change
    
    ---------
    
    Co-authored-by: AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>

--- src/main/java/me/wolfie/methane/mixin/BackgroundMixin.java
+++ src/main/java/me/wolfie/methane/mixin/BackgroundMixin.java
@@ -2,7 +2,6 @@ package me.wolfie.methane.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import me.wolfie.methane.Methane;
-import me.wolfie.methane.MethaneSettings;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.*;
 import net.minecraft.entity.Entity;
@@ -21,7 +20,7 @@ public class BackgroundMixin {
      */
     @Inject(method = "applyFog", at = @At("HEAD"), cancellable = true)
     private static void applyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
-        if (!Methane.ModActive && !MethaneSettings.persistFogSettings) return;
+        if (!Methane.ModActive && !Methane.settings.persistFogSettings) return;
 
         if (hasBlindOrDark(camera.getFocusedEntity())) return;
 
@@ -34,12 +33,12 @@ public class BackgroundMixin {
 
     @Unique
     private static boolean shouldDisableFog(CameraSubmersionType submersionType, boolean thickFog, BackgroundRenderer.FogType fogType) {
-        if (submersionType == CameraSubmersionType.NONE && MethaneSettings.disableAirFog) return true;
-        if (submersionType == CameraSubmersionType.WATER && MethaneSettings.disableWaterFog) return true;
-        if (submersionType == CameraSubmersionType.LAVA && MethaneSettings.disableLavaFog) return true;
-        if (submersionType == CameraSubmersionType.POWDER_SNOW && MethaneSettings.disablePowderedSnowFog) return true;
-        if (thickFog && MethaneSettings.disableThickFog) return true;
-        if (fogType == BackgroundRenderer.FogType.FOG_SKY && MethaneSettings.disableSkyFog) return true;
+        if (submersionType == CameraSubmersionType.NONE && Methane.settings.disableAirFog) return true;
+        if (submersionType == CameraSubmersionType.WATER && Methane.settings.disableWaterFog) return true;
+        if (submersionType == CameraSubmersionType.LAVA && Methane.settings.disableLavaFog) return true;
+        if (submersionType == CameraSubmersionType.POWDER_SNOW && Methane.settings.disablePowderedSnowFog) return true;
+        if (thickFog && Methane.settings.disableThickFog) return true;
+        if (fogType == BackgroundRenderer.FogType.FOG_SKY && Methane.settings.disableSkyFog) return true;
         return false;
     }
 
--- src/main/java/me/wolfie/methane/server/MethaneServerError.java
+++ src/main/java/me/wolfie/methane/server/MethaneServerError.java
@@ -1,14 +1,11 @@
 package me.wolfie.methane.server;
 
+import me.wolfie.methane.Methane;
 import net.fabricmc.api.DedicatedServerModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MethaneServerError implements DedicatedServerModInitializer {
-    public static Logger MethaneLogger = LoggerFactory.getLogger("Methane");
-
     @Override
     public void onInitializeServer() {
-        MethaneLogger.error("Methane is running on the server! This mod is client-only! Consider removing Methane from your server.");
+        Methane.MethaneLogger.error("Methane is running on the server! This mod is client-only! Consider removing Methane from your server.");
     }
 }
