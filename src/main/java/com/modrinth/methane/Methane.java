5a31fc5f9fb9b324c0203009c40d4fbe5fae4317
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2023-09-27 14:56:30 +1000

    feat: implement Methane Server Utilities Read more at: https://github.com/AnOpenSauceDev/Methane-Server-Utils

--- src/main/java/com/modrinth/methane/Methane.java
+++ src/main/java/com/modrinth/methane/Methane.java
@@ -1,19 +1,31 @@
 package com.modrinth.methane;
 
+import com.modrinth.methane.client.MethaneClient;
+import com.modrinth.methane.client.MethaneJoinPopUp;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import com.modrinth.methane.client.HudRenderListener;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static net.minecraft.client.realms.task.LongRunningTask.setScreen;
+
 public class Methane implements ModInitializer {
+
+
 
     public static boolean ModActive = true; // for toggles
 
     public static Logger MethaneLogger = LoggerFactory.getLogger("Methane");
     public static MethaneSettings settings;
+
+    public static final Identifier METHANE_STATE_PACKET = new Identifier("methane_server","statepacket");
+
     @Override
     public void onInitialize() {
         MethaneLogger.info("Methane has loaded!");
@@ -21,6 +33,24 @@ public class Methane implements ModInitializer {
         settings = AutoConfig.getConfigHolder(MethaneSettings.class).getConfig();
 
         HudRenderCallback.EVENT.register(new HudRenderListener());
+
+        ClientPlayNetworking.registerGlobalReceiver(METHANE_STATE_PACKET,((client, handler, buf, responseSender) -> {
+            MethaneLogger.info("recived packet from server");
+            int[] data = buf.readIntArray(); // 0 = enforceModState, 1 = globalModState, 2 = forceMethane (won't ever be used)
+            if(intToBoolConversion(data[0])){
+            MethaneClient.ToggleMethaneSetBool(client,intToBoolConversion(data[1]));
+            MethaneLogger.info("forcing methane config");
+            }else {
+                MethaneLogger.info("Methane settings prompt open");
+                setScreen(new MethaneJoinPopUp(Text.of("Methane Server Settings"), intToBoolConversion(data[1])));
+            }
+        }));
+    }
+
+
+
+    public boolean intToBoolConversion(int i){
+        return i != 0; // if "i" is not zero, return true
     }
 
 }
