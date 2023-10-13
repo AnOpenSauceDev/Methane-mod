4adeea36dda89692d7f917e63dcf88a5f74633d5
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2023-10-13 21:11:14 +1000

    Optimisation: don't worry about the biome for rain

--- src/main/java/com/modrinth/methane/mixin/DestructiveWeatherOptimisation.java
+++ src/main/java/com/modrinth/methane/mixin/DestructiveWeatherOptimisation.java
@@ -1,4 +1,30 @@
 package com.modrinth.methane.mixin;
 
-public class DestructiveWeatherOptimisation {
+
+import com.modrinth.methane.Methane;
+import net.fabricmc.fabric.api.client.rendering.v1.DimensionRenderingRegistry;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.WorldRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldRenderer.class)
+public class DestructiveWeatherOptimisation { // Dragons Beware!
+
+    @Inject(method = "renderWeather", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBiome(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/registry/entry/RegistryEntry;"),cancellable = true)
+    public void biomeHacks(LightmapTextureManager manager, float tickDelta, double cameraX, double cameraY, double cameraZ, CallbackInfo ci){
+        if(Methane.ModActive) ci.cancel();
+    }
+
+    @Inject(method = "renderWeather", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/biome/Biome;hasPrecipitation()Z"),cancellable = true)
+    public void biomeHacksPt2(LightmapTextureManager manager, float tickDelta, double cameraX, double cameraY, double cameraZ, CallbackInfo ci){
+        if(Methane.ModActive) rtrue(); // Before anyone asks me this, the rain splashing is ticked separately, thus causing the weird biome-dependent splashing effects. (which i don't really want to remove, but who knows)
+    }
+
+    public boolean rtrue(){
+        return  true;
+    }
+
 }
