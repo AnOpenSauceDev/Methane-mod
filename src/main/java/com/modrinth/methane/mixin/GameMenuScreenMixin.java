cac90b8f66a7f5b3009e280aa21710c5013cf2f1
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2023-11-26 09:02:29 +1000

    Take priority over other mods

--- src/main/java/com/modrinth/methane/mixin/GameMenuScreenMixin.java
+++ src/main/java/com/modrinth/methane/mixin/GameMenuScreenMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GameMenuScreen.class)
+@Mixin(value = GameMenuScreen.class, priority = 2000)
 public class GameMenuScreenMixin {
 
 
